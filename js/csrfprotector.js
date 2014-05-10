56c5ffc41e9a04f919fac90996509c111545192a
minhaz <minhazav@gmail.com>
2014-05-10 15:00:35 +0530

    CSRFP js modified
    #Modification:
    1. URL rewriting implemented for static URLS
    2. XHR method modified, now sends X-No-CSRF header with every POST
    requests, works for POST only currently

--- js/csrfprotector.js
+++ js/csrfprotector.js
@@ -1,13 +1,7 @@
-/**
- * This is javascript code for CSRF Protector. 
+/** 
+ * javascript code for OWASP CSRF Protector
  */
 
-
-/**
- * stores authkey in memory once it has been retreived from cookie
- * data-type string
- */
-var authKey = null;
 
 for(var i = 0; i<document.forms.length; i++) {
 	document.forms[i].onsubmit = function(evt) {
@@ -18,6 +12,10 @@ for(var i = 0; i<document.forms.length; i++) {
 	};
 }
 
+
+/** 
+ * function to get Auth key from cookie and return it to requesting function
+ */
 function getAuthKey() {
 	var re = new RegExp("CSRF_AUTH_TOKEN=([^;]+)(;|$)");
 	if(authKey === null) {
@@ -27,7 +25,9 @@ function getAuthKey() {
 	return authKey;
 }
 
-
+/** 
+ * function to get domain of any url
+ */
 function getDomain(url) {
 	// proxy doesn't work on https anyway
 	if (url.indexOf("http://") !== 0)
@@ -35,15 +35,27 @@ function getDomain(url) {
 	return /http:\/\/([^\/]+)/.exec(url)[1];
 }
 
+
+//===============================================================
+// Writing wrapper for XMLHttpRequest
+// Set X-No-CSRF to true before sending if request method is POST
+//===============================================================
+
+//add a property method to XMLHttpRequst class
 function new_open(method, url, async, username, password) {
+	this.method = method;
 	return this.old_open(method, url, async, username, password);
 }
+
+//currently functional for POST requests only
 function new_send(data) {
-   if (this.method === "POST") {
-      this.setRequestHeader("X-No-Csrf", "true");
-   }
-   data += "&CSRFPROTECTOR_AUTH_TOKEN=" +getAuthKey();
-   this.old_send(data);
+	if (this.method === "POST") {
+		this.setRequestHeader("X-No-CSRF", "true");
+		if(data.length !== 0)
+   		data += "&";
+    	data += "CSRFPROTECTOR_AUTH_TOKEN=" +getAuthKey();
+    }
+    return this.old_send(data);
 }
 
 
@@ -51,3 +63,27 @@ XMLHttpRequest.prototype.old_send = XMLHttpRequest.prototype.send;
 XMLHttpRequest.prototype.old_open = XMLHttpRequest.prototype.open;
 XMLHttpRequest.prototype.open = new_open;
 XMLHttpRequest.prototype.send = new_send;
+
+//================================================================
+// Rewrite existing urls for CSRF token
+// Rules:
+// Ingore cross origin urls & internal links (one with hashtags)
+// Append the token to those url already containig GET query parameter(s)
+// Add the token to those which does not contain GET query parameter(s)
+//================================================================
+for (var i = 0; i<document.links.length; i++) {
+	if(getDomain(document.links[i].href).indexOf(document.domain) === -1) {
+		//cross origin -- ignore
+		continue;
+	} else if (document.links[i].href.indexOf('#') !== -1) {
+		//hash tag | internal link -- ignore
+		continue;
+	} else if (document.links[i].href.indexOf('?') !== -1) {
+		document.links[i].href += "&CSRFPROTECTOR_AUTH_TOKEN=" +getAuthKey();
+	} else {
+		if (document.links[i].href[document.links[i].href.length - 1] != '/') {
+			document.links[i].href += '/';
+		}
+		document.links[i].href += "?CSRFPROTECTOR_AUTH_TOKEN=" +getAuthKey();
+	}
+}
