f1d63cb5ce92c7c96dfebc0d97bbd4849cd10f4b
minhaz <minhazav@gmail.com>
2014-06-15 17:53:35 +0530

    js code refactoring done
    #modification:
    1. Created class to hold basic functions
    2. form onsubmit now loads token dynamically
    3. In case of anchor tags no token is added is rules doesn't match the
    url

--- js/csrfprotector.php
+++ js/csrfprotector.php
@@ -7,91 +7,107 @@
  * =================================================================
  */
 
-// Array of patterns of url, for which csrftoken need to be added
-// In case of GET request also, provided from server
-var checkForUrls = new Array(<?php
-	if (isset($_GET['param'])) {
-		$patternArray = json_decode($_GET['param'],false);
-		if ($patternArray) {
-			foreach ($patternArray as $key => $value) {
-				if ($key !== 0) {
-					echo ',';
+var CSRFP = {
+	/**
+	 * Array of patterns of url, for which csrftoken need to be added
+	 * In case of GET request also, provided from server
+	 *
+	 * @var string array
+	 */
+	checkForUrls: [<?php
+		if (isset($_GET['param'])) {
+			$patternArray = json_decode($_GET['param'],false);
+			if ($patternArray) {
+				foreach ($patternArray as $key => $value) {
+					if ($key !== 0) echo ',';
+					echo "'". $value ."'";
 				}
-				echo "'". $value ."'";
 			}
 		}
-	}
-?>);
-
-//convert these rules to regex objects
-for (var i = 0; i < checkForUrls.length; i++) {
-	checkForUrls[i] = checkForUrls[i].replace(/\*/g, '(.*)')
-						.replace(/\//g, "\\/");
-	checkForUrls[i] = new RegExp(checkForUrls[i]);
-}
-
-/**
- * Function to check if a certain url is allowed to perform the request
- * With or without csrf token
- * @param: string, url
- * @return: boolean, 	true if csrftoken is not needed
- * 						false if csrftoken is needed
- */
-function isValidGetRequest(url) {
-	for (var i = 0; i < checkForUrls.length; i++) {
-		var match = checkForUrls[i].exec(url);
-		if (match !== null && match.length > 0) {
+	?>],
+	/**
+	 * Function to check if a certain url is allowed to perform the request
+	 * With or without csrf token
+	 * @param: string, url
+	 * @return: boolean, 	true if csrftoken is not needed
+	 * 						false if csrftoken is needed
+	 */
+	_isValidGetRequest: function(url) {
+		for (var i = 0; i < CSRFP.checkForUrls.length; i++) {
+			var match = CSRFP.checkForUrls[i].exec(url);
+			if (match !== null && match.length > 0) {
+				return false;
+			}
+		}
+		return true;
+	},
+	/** 
+	 * function to get Auth key from cookie Andreturn it to requesting function
+	 * @param: void
+	 * @return: string, csrftoken retrieved from cookie
+	 */
+	_getAuthKey: function() {
+		var re = new RegExp("CSRF_AUTH_TOKEN=([^;]+)(;|$)");
+		var RegExpArray = re.exec(document.cookie);
+		
+		if (RegExpArray === null) {
+			//#todo: Action to take if CSRFtoken not found
 			return false;
 		}
-	}
-	return true;
-}
-
-/** 
- * function to get Auth key from cookie Andreturn it to requesting function
- * @param: void
- * @return: string, csrftoken retrieved from cookie
- */
-function getAuthKey() {
-	var re = new RegExp("CSRF_AUTH_TOKEN=([^;]+)(;|$)");
-	var RegExpArray = re.exec(document.cookie);
+		return RegExpArray[1];
+	},
+	/** 
+	 * Function to get domain of any url
+	 * @param: string, url
+	 * @return: string, domain of url
+	 */
+	_getDomain: function(url) {
+		if (url.indexOf("http://") !== 0 && url.indexOf("https://") !== 0)
+			return document.domain;
+		return /http(s)?:\/\/([^\/]+)/.exec(url)[2];
+	},
+	/**
+	 * Initialises the CSRFProtector js script
+	 *
+	 * @param void
+	 * @return void
+	 */
+	_init: function() {
+		//convert these rules received from php lib to regex objects
+		for (var i = 0; i < CSRFP.checkForUrls.length; i++) {
+			CSRFP.checkForUrls[i] = CSRFP.checkForUrls[i].replace(/\*/g, '(.*)')
+								.replace(/\//g, "\\/");
+			CSRFP.checkForUrls[i] = new RegExp(CSRFP.checkForUrls[i]);
+		}
 	
-	if (RegExpArray === null) {
-		//#todo: Action to take if CSRFtoken not found
-		return false;
 	}
-	return RegExpArray[1];
-}
-
-/** 
- * Function to get domain of any url
- * @param: string, url
- * @return: string, domain of url
- */
-function getDomain(url) {
-	if (url.indexOf("http://") !== 0 && url.indexOf("https://") !== 0)
-		return document.domain;
-	return /http(s)?:\/\/([^\/]+)/.exec(url)[2];
-}
+	
+}; 
 
 //==========================================================
 // Adding tokens, wrappers on window onload
 //==========================================================
 
 window.onload = function() {
-
+	
+	// Call the init funcion
+	CSRFP._init();
+	
 	//==================================================================
 	// Adding csrftoken to request resulting from <form> submissions
 	// Add for each POST, while for mentioned GET request
 	//==================================================================
 	for(var i = 0; i < document.forms.length; i++) {
 		document.forms[i].onsubmit = function(event) {
-			if (!event.target.csrfp_token) {
+			if (typeof event.target.csrfp_token === 'undefined') {
 				var hiddenObj = document.createElement("input");
 				hiddenObj.name = 'csrfp_token';
 				hiddenObj.type = 'hidden';
-				hiddenObj.value = getAuthKey();
+				hiddenObj.value = CSRFP._getAuthKey();
 				event.target.appendChild(hiddenObj);
+			} else {
+				//modify token to latest value
+				event.target.csrfp_token.value = CSRFP._getAuthKey();
 			}
 		};
 	}
@@ -111,12 +127,12 @@ window.onload = function() {
 	function new_open(method, url, async, username, password) {
 		this.method = method;
 
-		if (method.toLowerCase() === 'get' && !isValidGetRequest(url)) {
+		if (method.toLowerCase() === 'get' && !CSRFP._isValidGetRequest(url)) {
 			//modify the url
 			if (url.indexOf('?') === -1) {
-				url += "?csrfp_token=" +getAuthKey();
+				url += "?csrfp_token=" +CSRFP._getAuthKey();
 			} else {
-				url += "&csrfp_token" +getAuthKey();
+				url += "&csrfp_token" +CSRFP._getAuthKey();
 			}
 		}
 
@@ -141,7 +157,7 @@ window.onload = function() {
 				data = "";
 			}
 			
-			data += "csrfp_token=" +getAuthKey();
+			data += "csrfp_token=" +CSRFP._getAuthKey();
 		}
 		return this.old_send(data);
 	}
@@ -162,28 +178,25 @@ window.onload = function() {
 	//==================================================================
 
 	for (var i = 0; i < document.links.length; i++) {
-
-	
         document.links[i].addEventListener("mousedown", function(event) {
             var urlDisect = event.target.href.split('#');
             var url = urlDisect[0];
             var hash = urlDisect[1];
-            
-            if(getDomain(url).indexOf(document.domain) === -1) {
-                //cross origin -- ignore
-            }
-            if (isValidGetRequest(url)) {
-                //not provided in rules
+			
+            if(CSRFP._getDomain(url).indexOf(document.domain) === -1
+				|| CSRFP._isValidGetRequest(url)) {
+                //cross origin -- ignore or not to be protected by rules
+				return;
             }
             
             if (url.indexOf('?') !== -1) {
                 if(url.indexOf('csrfp_token') === -1) {
-                    url += "&csrfp_token=" +getAuthKey();
+                    url += "&csrfp_token=" +CSRFP._getAuthKey();
                 } else {
-                    url = url.replace(new RegExp("csrfp_token=.*?(&|$)", 'g'), "csrfp_token="+getAuthKey() + "$1");
+                    url = url.replace(new RegExp("csrfp_token=.*?(&|$)", 'g'), "csrfp_token=" +CSRFP._getAuthKey() + "$1");
                 }
             } else {
-                url += "?csrfp_token=" +getAuthKey();
+                url += "?csrfp_token=" +CSRFP._getAuthKey();
             }
             
             event.target.href = url;
