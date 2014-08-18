b820e93bac091a991acc7eb9ea352d29fb5aedd2
minhaz <minhazav@gmail.com>
2014-08-18 16:38:08 +0530

    files corrected. logs removed

--- js/csrfprotector.js
+++ js/csrfprotector.js
@@ -19,7 +19,7 @@ var CSRFP = {
 	 *
 	 * @var string array
 	 */
-	checkForUrls: ['http://test/index*'],
+	checkForUrls: [],
 	/**
 	 * Function to check if a certain url is allowed to perform the request
 	 * With or without csrf token
